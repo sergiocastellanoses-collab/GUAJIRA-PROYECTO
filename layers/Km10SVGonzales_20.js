e4b24ed7b8ba9277c358dedfb1a9e49c6782de11
sergiocastellanoses-collab <sergio.castellanoses@gmail.com>
2025-10-29 08:30:50 -0500

    Update Km10SVGonzales_20.js

--- layers/Km10SVGonzales_20.js
+++ layers/Km10SVGonzales_20.js
@@ -17,14 +17,14 @@ var json_Km10SVGonzales_20 = {
           11°27’1.47” N 72°16’5.18” W
           <br><br>
           <div style="text-align:center; margin-top:8px;">
-            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=3lqqrF" 
+            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=gJqI85" 
                target="_blank">
               <img src="images/Km10 0+000.jpg" 
                    style="max-width:50px; height:auto; border-radius:8px; box-shadow:0px 0px 6px rgba(0,0,0,0.3);" 
                    alt="Imagen Km10 0+000">
             </a>
             <br>
-            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=3lqqrF" 
+            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=gJqI85" 
                target="_blank" 
                style="display:block; text-align:center; margin-top:6px; font-weight:bold; color:#2a4d69; text-decoration:none;">
                VER MÁS
@@ -47,14 +47,14 @@ var json_Km10SVGonzales_20 = {
           11°27’12.60” N 72°16’29.39” W
           <br><br>
           <div style="text-align:center; margin-top:8px;">
-            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=3lqqrF" 
+            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=gJqI85" 
                target="_blank">
               <img src="images/Km10 0+816.jpg" 
                    style="max-width:50px; height:auto; border-radius:8px; box-shadow:0px 0px 6px rgba(0,0,0,0.3);" 
                    alt="Imagen Km10 0+816">
             </a>
             <br>
-            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=3lqqrF" 
+            <a href="https://1drv.ms/p/c/e9fcf6d0213b42a0/EaSCHW_mRfdKqSWsPS0kDk8BkX6nDYJUn-TjBb1WH5en2A?e=gJqI85" 
                target="_blank" 
                style="display:block; text-align:center; margin-top:6px; font-weight:bold; color:#2a4d69; text-decoration:none;">
                VER MÁS
