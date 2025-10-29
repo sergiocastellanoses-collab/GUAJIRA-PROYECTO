0fffa5c02fa0558fcbeb8a47b3f211c1cd4605a2
sergiocastellanoses-collab <sergio.castellanoses@gmail.com>
2025-10-29 08:00:27 -0500

    Update VaantiguaUribia_18.js

--- layers/VaantiguaUribia_18.js
+++ layers/VaantiguaUribia_18.js
@@ -17,13 +17,13 @@ var json_VaantiguaUribia_18 = {
           11°26’14.13” N 72°16’0.10” W
           <br><br>
           <div style="text-align:center; margin-top:8px;">
-            <a href="presentacion/ANTIGUA URIBIA.ppx" target="_blank">
+            <a href="presentacion/ANTIGUA URIBIA.pptx" target="_blank">
               <img src="images/Km uribia 0+806.jpg" 
                    style="max-width:50px; height:auto; border-radius:8px; box-shadow:0px 0px 6px rgba(0,0,0,0.3);" 
                    alt="Imagen Km uribia 0+806">
             </a>
             <br>
-            <a href="presentacion/ANTIGUA URIBIA.ppx" 
+            <a href="presentacion/ANTIGUA URIBIA.pptx" 
                target="_blank" 
                style="display:block; text-align:center; margin-top:6px; font-weight:bold; color:#2a4d69; text-decoration:none;">
                VER MÁS
@@ -43,13 +43,13 @@ var json_VaantiguaUribia_18 = {
           11°26’25.56” N 72°16’1.25” W
           <br><br>
           <div style="text-align:center; margin-top:8px;">
-            <a href="presentacion/ANTIGUA URIBIA.ppx" target="_blank">
+            <a href="presentacion/ANTIGUA URIBIA.pptx" target="_blank">
               <img src="images/Km uribia vaso1.jpg" 
                    style="max-width:50px; height:auto; border-radius:8px; box-shadow:0px 0px 6px rgba(0,0,0,0.3);" 
                    alt="Imagen Km uribia vaso1">
             </a>
             <br>
-            <a href="presentacion/ANTIGUA URIBIA.ppx" 
+            <a href="presentacion/ANTIGUA URIBIA.pptx" 
                target="_blank" 
                style="display:block; text-align:center; margin-top:6px; font-weight:bold; color:#2a4d69; text-decoration:none;">
                VER MÁS
@@ -69,13 +69,13 @@ var json_VaantiguaUribia_18 = {
           11°26’30.18” N 72°16’1.76” W
           <br><br>
           <div style="text-align:center; margin-top:8px;">
-            <a href="presentacion/ANTIGUA URIBIA.pdf" target="_blank">
+            <a href="presentacion/ANTIGUA URIBIA.pptx" target="_blank">
               <img src="images/Km uribia vaso2.jpg" 
                    style="max-width:50px; height:auto; border-radius:8px; box-shadow:0px 0px 6px rgba(0,0,0,0.3);" 
                    alt="Imagen Km uribia vaso2">
             </a>
             <br>
-            <a href="presentacion/ANTIGUA URIBIA.pdf" 
+            <a href="presentacion/ANTIGUA URIBIA.pptx" 
                target="_blank" 
                style="display:block; text-align:center; margin-top:6px; font-weight:bold; color:#2a4d69; text-decoration:none;">
                VER MÁS
@@ -95,13 +95,13 @@ var json_VaantiguaUribia_18 = {
           11°26’40.23” N 72°16’2.84” W
           <br><br>
           <div style="text-align:center; margin-top:8px;">
-            <a href="presentacion/ANTIGUA URIBIA.pdf" target="_blank">
+            <a href="presentacion/ANTIGUA URIBIA.pptx" target="_blank">
               <img src="images/Km uribia 0+000.jpg" 
                    style="max-width:50px; height:auto; border-radius:8px; box-shadow:0px 0px 6px rgba(0,0,0,0.3);" 
                    alt="Imagen Km uribia 0+000">
             </a>
             <br>
-            <a href="presentacion/ANTIGUA URIBIA.ppx" 
+            <a href="presentacion/ANTIGUA URIBIA.pptx" 
                target="_blank" 
                style="display:block; text-align:center; margin-top:6px; font-weight:bold; color:#2a4d69; text-decoration:none;">
                VER MÁS
